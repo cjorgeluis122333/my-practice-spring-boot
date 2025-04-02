6144e39bc1bcba40d653403ac969f51fb4494f1a
jcvidal <jcvidal@gmail.com>
2025-04-02 17:08:03 -0400

    Create project

---
+++ src/main/java/com/microservice/eureca/my_practice_springboot/MyPracticeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.eureca.my_practice_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPracticeSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPracticeSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservice/eureca/my_practice_springboot/MyPracticeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.eureca.my_practice_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPracticeSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
